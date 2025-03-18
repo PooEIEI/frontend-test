fa4df2c0440e6e93875e4e1ea6d57c882157d539
PooEIEI <poopoo18273@gmail.com>
2025-03-18 15:28:57 +0700

    eee

--- part2/script.js
+++ part2/script.js
@@ -5,13 +5,11 @@ const total = items.length;
 let autoPlay = true;
 let interval;
 
-// ตั้งค่าให้การเลื่อนลื่นไหล
 function updateSlide() {
     slider.style.transition = "transform 0.5s ease-in-out";
     slider.style.transform = `translateX(${-index * 320}px)`;
 }
 
-// ฟังก์ชันเลื่อนขวา
 function nextSlide() {
     if (index >= total - 1) {
         index = 0;
@@ -21,7 +19,6 @@ function nextSlide() {
     updateSlide();
 }
 
-// ฟังก์ชันเลื่อนซ้าย
 function prevSlide() {
     if (index <= 0) {
         index = total - 1;
@@ -31,12 +28,10 @@ function prevSlide() {
     updateSlide();
 }
 
-// Auto Slide
 function startAutoSlide() {
     interval = setInterval(nextSlide, 3000);
 }
 
-// หยุด Auto Slide เมื่อ hover
 slider.addEventListener("mouseenter", () => clearInterval(interval));
 slider.addEventListener("mouseleave", startAutoSlide);
 
